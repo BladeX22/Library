8184753c5a51c5776e88f16459c661a595ba667b
rafal <rafal.najczuk@mokyokit.com>
2016-10-20 13:49:31 +0200

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'navigation',
+    templateUrl: 'app/navigation.html',
+    styleUrls: [ 'app/library-homepage.css' ]
+})
+export class AppComponent { }
---
+++ app/content.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'page_content',
+    templateUrl: 'app/content.html',
+    styleUrls: [ 'app/library-homepage.css' ]
+})
+export class ContentComponent { }
